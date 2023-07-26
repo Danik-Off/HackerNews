e4dfd0367b0da5f325b24332437af3a01744de74
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-07-26 18:21:35 +0300

    Update NewsDetail.jsx

--- src/views/NewsDetail.jsx
+++ src/views/NewsDetail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { useParams,useNavigate } from "react-router-dom";
 
 const NewsDetail = () => {
@@ -6,13 +6,52 @@ const NewsDetail = () => {
   const navigate = useNavigate();
   
   const params = useParams();
-  const prodId = params.id;
+  const id= params.id;
+
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchNewsItem = async () => {
+      try {
+        const response = await fetch(
+          `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+        );
+        if (!response.ok) {
+          throw new Error("Ошибка при выполнении запроса");
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Произошла ошибка:", error);
+      }
+    };
+
+    fetchNewsItem();
+  }, []);
 
   return (
     <>
     <button onClick={() => navigate("/")}>Назад</button>
     <hr></hr>
-      <h2>Новость {prodId}</h2>
+    {data ? (
+        <>
+          <div>
+            <h2>{data.title}</h2>
+            <p>{data.text}</p>
+            <div className="details">
+              <span className="like">like {data.score}</span> | by 
+              <span className="author">{data.by}</span> |  
+              <span className="date">
+                {new Date(data.time * 1000).toLocaleDateString()}
+              </span>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Загрузка...</p>
+      )}
+  
     </>
   );
 };
