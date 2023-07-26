4511d52d11cc5af98bbe7f7b88152bc5befe9dea
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-07-26 20:40:28 +0300

    Update CommentsTree.jsx

--- src/components/CommentsTree.jsx
+++ src/components/CommentsTree.jsx
@@ -1,28 +1,96 @@
-import React from "react";
-import "./CommentsTree.scss"; // Импорт файла со стилями
+import React, { useState, useEffect } from "react";
+import "./CommentsTree.scss";
+import { Caching } from "../scripts/Caching";
 
-const CommentsTree = ({ comments }) => {
+const CommentsTree = ({ kidsIds }) => {
+  const [count, setCount] = useState(0);
+
+  const fetchNewsItem = async (id) => {
+    try {
+      const response = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+      );
+      if (!response.ok) {
+        throw new Error("Ошибка при выполнении запроса");
+      }
+      const data = await response.json();
+      Caching("comment" + id, data);
+      if (data.kids) {
+        setCount((prevCount) => prevCount + data.kids.length);
+        for (let child of data.kids) {
+          fetchNewsItem(child);
+        }
+      }
+    } catch (error) {
+      console.error("Произошла ошибка:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (kidsIds) {
+      setCount(kidsIds.length);
+      for (let child of kidsIds) {
+        fetchNewsItem(child);
+      }
+    }
+  }, [kidsIds]);
+
   return (
-    <ul className="comments">
-      {comments.map((comment) => (
-        <Comment key={comment.id} comment={comment} />
-      ))}
-    </ul>
+    <>
+      <h3>Комментарии</h3>
+      {count}
+      <ul className="comments">
+        {kidsIds ? (
+          kidsIds.map((commentId) => <Comment key={commentId} id={commentId} />)
+        ) : (
+          <div>Комментариев нет</div>
+        )}
+      </ul>
+    </>
   );
 };
 
-const Comment = ({ comment }) => {
-  const { id, text, author, time, children } = comment;
+const Comment = ({ id }) => {
+  const [data, setData] = useState(null);
+
+  const fetchNewsItem = async () => {
+    try {
+      const response = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+      );
+      if (!response.ok) {
+        throw new Error("Ошибка при выполнении запроса");
+      }
+      const data = await response.json();
+      setData(data);
+      Caching("comment" + id, data);
+    } catch (error) {
+      console.error("Произошла ошибка:", error);
+    }
+  };
+
+  useEffect(() => {
+    const cach = Caching("comment" + id);
+    cach ? setData(cach) : fetchNewsItem();
+  }, [id]);
 
   return (
     <li className="comment">
-      <div className="comment-text">{text}</div>
-      <div className="comment-author">{author}</div>
-      <div className="comment-time">{time}</div>
-      {children && children.length > 0 && (
-        <ul className="comment-children">
-          <CommentsTree comments={children} />
-        </ul>
+      {data ? (
+        <>
+          <p dangerouslySetInnerHTML={{ __html: data.text }}></p>
+          <div className="comment-author">{data.by}</div>
+          <div className="comment-time">
+            {new Date(data.time * 1000).toLocaleDateString()}
+          </div>
+          {data.kids && data.kids.length > 0 && (
+            <ul className="comment-children">
+              <CommentsTree kidsIds={data.kids} />
+            </ul>
+          )}
+        </>
+      ) : (
+        <div className="updateWin">Загрузка</div>
       )}
     </li>
   );
